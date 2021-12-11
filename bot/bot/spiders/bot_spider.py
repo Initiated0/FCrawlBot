36a865dc7ccc25dae7dc317bf435388238b97834
initiate0 <damonmayeehamm@gmail.com>
2021-12-11 13:07:25 +0600

    trying to scape banglasaroma

--- bot/bot/spiders/bot_spider.py
+++ bot/bot/spiders/bot_spider.py
@@ -8,19 +8,29 @@ class BotSpider(scrapy.Spider):
         # 'https://blog.scrapinghub.com/page/2/ 
         # 'https://www.zyte.com/blog/page/2'
         # 'https://www.samsung.com/us/'
-        # 'https://www.facebook.com/nayeem.mohammad1/'      
+        # 'https://www.facebook.com/nayeem.mohammad1/'
+        'https://www.bengalsaroma.com/category/blog/page/2/'
+        'https://www.bengalsaroma.com/category/blog/page/3/'      
+      
     ]
-
     def parse(self, response):
-        for post in response.css('div.post-item'):
-            yield{
-                'title' : post.css('.post-header h2 a::text')[0].get(),
-                'date' : post.css('.post-header a::text')[1].get(),
-                'author' : post.css('.post-header a::text')[2].get(),
-            }
-        next_page = response.css('a.next-posts-link::attr(href').get()
-        if next_page is not None:
-            next_page = response.urljoin(next_page)
-            yield scrapy.Request(next_page, callback-self.parse)
-            # run scrapy crawl posts -o posts.json
+        page = response.url.split('/')[-1]
+        filename = 'entry-%s.html' % page
+        with open(filename, 'wb') as f:
+            f.write(response.body)
+            # run this with scrapy shell https://www.banglasaroma.com/category/blog/
+
+
+    # def parse(self, response):
+    #     for post in response.css('div.post-item'):
+    #         yield{
+    #             'title' : post.css('.entry-header h2 a::text')[0].get()
+    #             'date' : post.css('.post-header a::text')[1].get(),
+    #             'author' : post.css('.post-header a::text')[2].get(),
+    #         }
+    #     next_page = response.css('a.next-posts-link::attr(href').get()
+    #     if next_page is not None:
+    #         next_page = response.urljoin(next_page)
+    #         yield scrapy.Request(next_page, callback-self.parse)
+    #         # run scrapy crawl posts -o posts.json
         
